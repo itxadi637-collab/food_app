7c3a2ec6777953bab3d1eaed9592095aea2c54bf
itx <itxadi637@gmail.com>
2026-02-17 17:23:16 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'screens/forget_screen.dart';
 import 'homescreen/home_screen.dart';
 import 'package:food_app/screens/login_screen.dart';
 
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
